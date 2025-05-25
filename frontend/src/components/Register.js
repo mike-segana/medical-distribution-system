8a227461f1ef668b5b694e3407e15092dc28c5da
Mike Segana <ms02916@surrey.ac.uk>
2025-05-25 23:43:22 +0100

    Updated Code

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -7,10 +7,11 @@ function Register() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
+    const [role, setRole] = useState("");
 
     const register = async () => {
         try {
-            const res = await axios.post(`${AUTH_API_URL}/auth/`, { username, password, email });
+            const res = await axios.post(`${AUTH_API_URL}/auth/`, { username, password, email, role  });
             alert("Registration successful");
         } catch (err) {
             alert("Registration failed");
@@ -23,6 +24,7 @@ function Register() {
             <input id="username" name="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
             <input id="password" name="password" placeholder="Password" type="password" onChange={(e) => setPassword(e.target.value)} />
             <input id="email" name="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+            <input id="email" name="role" placeholder="Role" onChange={(e) => setRole(e.target.value)} />
             <button onClick={register}>Register</button>
             <p>Already have an account? <Link to="/">Login</Link></p>
         </div>
